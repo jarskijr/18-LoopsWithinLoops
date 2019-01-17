ef1fef1ff8fef3f21928d0bc926aeff077ce011d
jarskijr <jarskijr@rose-hulman.edu>
2019-01-17 09:20:50 -0500

    Done...ish

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -4,7 +4,7 @@ in the context of PRINTING on the CONSOLE.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
          their colleagues and Jacob Jarski.
-"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LIN.
 
 
 def main():
